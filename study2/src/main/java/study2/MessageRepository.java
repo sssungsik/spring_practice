ea4a545853ab4eb691c2acede2e8a5784b0731a5
최성식 <162602047+sssungsik@users.noreply.github.com>
2024-06-19 17:30:31 +0900

    feat : graddle 프로젝트 공부

---
+++ study2/src/main/java/study2/MessageRepository.java
@@ -0,0 +1,12 @@
+package study2;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    Message findByTitle(String title);
+    Message findByTitleAndContent(String title, String content);
+    List<Message> findByTitleLike(String title);
+
+}
